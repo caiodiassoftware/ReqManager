f7e65f67449ead5265ebee2c788833cffaf23ae7
Caio Dias <caiodias12@gmail.com>
2017-11-21 21:53:57 -0300

    TESTING FILTER USING jQuery.DataTable IN CHARACTERISTICS INDEX VIEW

--- ReqManager/Controllers/CharacteristicsController.cs
+++ ReqManager/Controllers/CharacteristicsController.cs
@@ -1,6 +1,11 @@
 ﻿using ReqManager.Entities.Project;
 using ReqManager.ManagerController;
 using ReqManager.Services.Estructure;
+using ReqManager.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
 
 namespace ReqManager.Controllers
 {
@@ -8,6 +13,40 @@ namespace ReqManager.Controllers
     {
         public CharacteristicsController(IService<CharacteristicsEntity> service) : base(service)
         {
+        }
+
+        public ActionResult GetFilter()
+        {
+            try
+            {
+                IEnumerable<CharacteristicsEntity> filtered = Service.getAll();
+
+                string filterName = Request.QueryString["name"];
+
+                if (!string.IsNullOrEmpty(filterName))
+                {
+                    filtered = filtered.Where(c => c.name.Contains(filterName)
+                               ||
+                    c.description.Contains(filterName)
+                               ||
+                               c.active.ToString().Contains(filterName));
+                }
+
+                var result = from c in filtered
+                             select new[] { c.name, c.description, c.active.ToString() };
+
+                return Json(new
+                {
+                    sEcho = filterName,
+                    iTotalRecords = 97,
+                    iTotalDisplayRecords = 3,
+                    aaData = result
+                }, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
         }
     }
 }
